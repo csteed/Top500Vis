6e0f623241f8281ab9c86cdcaed0836160cc12a2
Chad A. Steed <steedca@ornl.gov>
2020-05-27 23:23:28 -0400

    Added heatmap, officially

--- js/heatmapChart.js
+++ js/heatmapChart.js
@@ -2,9 +2,12 @@ var heatmapChart = function () {
   let margin = {top: 20, right: 20, bottom: 20, left: 20};
   let width = 960 - margin.left - margin.right;
   let height = 500 - margin.top - margin.bottom;
+  let rowHeight = 12;
 
-  var highlightStrings = [];
-  var chartData;
+  var rowFilterStrings = [];
+  var chartRows;
+  var chartDates;
+  var filteredRows;
   var chartDiv;
 
   let x;
@@ -12,7 +15,11 @@ var heatmapChart = function () {
   const formatDate = d3.timeFormat("%Y %b");
 
   function chart(selection, data) {
-    chartData = data;
+    chartDates = data.dates;
+    chartRows = data.rows;
+    filteredRows = rowFilterStrings.length > 0 ? chartRows.filter(d => isRowShowing(d.name)) : chartRows;
+    // filteredChartData = rowFilterStrings.length > 0 ? chartData.filter(d => isRowShowing(d.name)) : chartData;
+
     chartDiv = selection;
     drawChart();
   }
@@ -22,31 +29,32 @@ var heatmapChart = function () {
       chartDiv.selectAll('*').remove();
     }
 
-    if (chartData) {
-      console.log(`height: ${height}`);
-      console.log(`width: ${width}`);
-
+    if (filteredRows) {
+      height = filteredRows.length * rowHeight;
       const svg = chartDiv.append('svg')
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
         .append('svg:g')
           .attr("transform", `translate(${margin.left},${margin.top})`);
 
-      console.log(chartData);
       x = d3.scaleBand()
-        .domain(chartData.dates)
+        .domain(chartDates)
         .range([0, width]);
-      // console.log(x.domain());
-      // console.log(x.range());
-      // console.log(`${chartData.dates[0]} is at ${x(chartData.dates[0])}`);
 
       y = d3.scaleBand()
-        .domain(chartData.rows.map(d => d.name))
+        .domain(filteredRows.map(d => d.name))
         .range([0, height]);
-
+      
       // const color = d3.scaleSequentialSqrt([d3.max(chartData.rows, d => d3.max(d.values)), 1], d3.interpolateBuPu);
-      const color = d3.scaleSequentialSqrt(t => d3.interpolateOrRd((t + 0.25) / 2))
-          .domain([d3.max(chartData.rows, d => d3.max(d.values)), 1]);
+      // const color = d3.scaleSequentialSqrt(t => d3.interpolateOrRd((t + 0.25) / 2))
+      //     .domain([d3.max(chartData.rows, d => d3.max(d.values)), 1]);
+      // const color = d3.scaleSequential([1, 500], d3.interpolateOrRd());
+      // const color = d3.scaleSequential(
+      //   [d3.max(filteredRows, d => d3.max(d.values)), 1], 
+      //   t => d3.interpolateYlOrBr((t + 0.25) / 2));
+      const color = d3.scaleSequentialSqrt([d3.max(filteredRows, d => d3.max(d.values)), 1], t => d3.interpolateOrRd((t * .75 + 0.15)));
+      // const color = d3.scaleQuantize([1,500], d3.schemeBlues[9])
+      // console.log(d3.schemeOrRd[9]);
 
       svg.append("g")
         .attr("class", "axis axis--x")
@@ -59,20 +67,16 @@ var heatmapChart = function () {
           .attr("text-anchor", "start")
           .attr('transform', 'rotate(-90)'));
 
-      const truncateName = (name) => {
-        if (name.length > 20) {
-          return name.substring(0, 20) + "...";
-        }
-        return name;
-      }
       svg.append("g")
         .attr("class", "axis axis--y")
         .call(d3.axisLeft(y))
-        .call(g => g.select(".domain").remove());
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll("text")
+          .append("title").text(d => d));
 
       const row = svg.append("g")
           .selectAll("g")
-        .data(chartData.rows)
+        .data(filteredRows)
         .join("g")
           .attr("transform", d => `translate(0,${y(d.name)})`)
           .on('mouseover touchover', d => {
@@ -89,15 +93,18 @@ var heatmapChart = function () {
       row.selectAll("rect")
         .data(d => d.values)
         .join("rect")
-          .attr("x", (d, i) => x(chartData.dates[i]))
+          // .attr("rx", 3)
+          // .attr("ry", 3)
+          .attr("x", (d, i) => x(chartDates[i]))
           .attr("width", x.bandwidth() - 1)
           .attr("height", y.bandwidth() - 1)
-          .attr("fill", v => isNaN(v) ? "whitesmoke" : v === 1 ? "red" : color(v))
-          // .attr("stroke", v => v === 1 ? "dodgerblue" : null)
+          .attr("fill", v => isNaN(v) ? "whitesmoke" : color(v))
+          // .attr("fill", v => isNaN(v) ? "whitesmoke" : v === 1 ? d3.schemeBuPu[9][8] : color(v))
+          // .attr("stroke", v => v === 1 ? "black" : null)
           .on('mouseover touchover', (d, i) => {
             d3.select('.axis--x')
               .selectAll('text')
-              .attr('font-weight', t => t.getTime() === chartData.dates[i].getTime() ? 'bold' : null);
+              .attr('font-weight', t => t.getTime() === chartDates[i].getTime() ? 'bold' : null);
           })
           .on('mouseout touchout', d => {
             d3.select('.axis--x')
@@ -105,8 +112,50 @@ var heatmapChart = function () {
               .attr('font-weight', null);
           })
         .append("title")
-          .text((d, i) => `${d ? `#${d}` : 'No Ranking'} on ${formatDate(chartData.dates[i])}`);
+          .text((d, i) => `${d ? `#${d}` : 'No Ranking'} on ${formatDate(chartDates[i])}`);
+      
+      svg.append("g")
+        .attr("transform", `translate(${width - 300}, -${margin.top})`)
+        .append(() => legend({
+          color,
+          title: 'Top500 List Ranks',
+          width: width < 300 ? width : 300,
+          tickFormat: "d"
+          // tickValues: [1, 20, 50, 100, 200, 300, 400, 500]
+        })); 
     }
+  }
+
+  const isRowShowing = (name) => {
+    for (let i = 0; i < rowFilterStrings.length; i++) {
+      if (name.toLowerCase().includes(rowFilterStrings[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  chart.setRowFilterStrings = function (value) {
+    if (!arguments.length) {
+      return rowFilterStrings;
+    }
+
+    let strings = d3.csvParseRows(value)[0];
+    rowFilterStrings = [];
+    if (strings) {
+      strings.forEach( s => {
+        if (s.trim().length > 0) {
+          rowFilterStrings.push(s.trim().toLowerCase());
+        }
+      });
+    }
+
+    if (chartRows) {
+      filteredRows = rowFilterStrings.length > 0 ? chartRows.filter(d => isRowShowing(d.name)) : chartRows;
+      drawChart();
+    }
+
+    return chart;
   }
 
   chart.margin = function(value) {
@@ -114,10 +163,10 @@ var heatmapChart = function () {
       return margin;
     }
     oldChartWidth = width + margin.left + margin.right;
-    oldChartHeight = height + margin.top + margin.bottom;
+    // oldChartHeight = height + margin.top + margin.bottom;
     margin = value;
     width = oldChartWidth - margin.left - margin.right;
-    height = oldChartHeight - margin.top - margin.bottom;
+    // height = oldChartHeight - margin.top - margin.bottom;
     drawChart();
     return chart;
   };
@@ -131,11 +180,12 @@ var heatmapChart = function () {
     return chart;
   };
 
-  chart.height = function(value) {
+  chart.rowHeight = function(value) {
     if (!arguments.length) {
-      return height;
+      return rowHeight;
     }
-    height = value - margin.top - margin.bottom;
+    rowHeight = value;
+    // height = value - margin.top - margin.bottom;
     drawChart();
     return chart;
   };
